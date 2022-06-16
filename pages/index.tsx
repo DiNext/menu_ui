7983f47e4c6b94d595432aef25c2fab921fe43df
DiNext <qwertik0996@gmail.com>
2022-06-16 09:41:00 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ ConfigProvider.config({
 
 function Main()  {
   const fetcher = (url: any) => axios.get(url).then(res => res.data);
-  const { data, error } = useSWR('http://216556.fornex.cloud/api/category', fetcher);
+  const { data, error } = useSWR('http://127.0.0.1/api/category', fetcher);
   const [count, setCount] = useState(0);
   const [visible, setVisible] = useState(false);
   const [backetData, setBacketData] = useState([]);
