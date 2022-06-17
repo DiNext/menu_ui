1d779bd15ff213017ea36b743e7d22638e06a55a
DiNext <qwertik0996@gmail.com>
2022-06-17 23:46:19 +0600

    1

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ ConfigProvider.config({
 
 function Main()  {
   const fetcher = (url: any) => axios.get(url).then(res => res.data);
-  const { data, error } = useSWR('http://localhost:3001/api/category', fetcher);
+  const { data, error } = useSWR('https://localhost:3001/api/category', fetcher);
   const [count, setCount] = useState(0);
   const [visible, setVisible] = useState(false);
   const [backetData, setBacketData] = useState([]);
